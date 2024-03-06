15df7ac5cbc61202ca3059439606fb34a6384bae
Uri Chandler <uri@ironblocks.com>
2024-03-06 23:05:16 +0200

    fix: updated fw tests

--- tests/firewall/firewall.command.spec.ts
+++ tests/firewall/firewall.command.spec.ts
@@ -1,65 +1,64 @@
 // Builtin.
-import * as assert from 'assert';
-// 3rd party.
 import { TestingModule } from '@nestjs/testing';
 import { CommandTestFactory } from 'nest-commander-testing';
-import * as sinon from 'sinon';
-import { SinonSandbox, SinonStub } from 'sinon';
-// Internal.
+
 import { AppModule } from '../../src/app/app.module';
-import { replaceSingleSpace } from '../lib/utils';
+import { DESCRIPTION, NAME } from '../../src/firewall/firewall.command.descriptor';
 
 
-describe('Firewall Command', () => {
-    let sandbox: SinonSandbox;
-    let exitStub: SinonStub;
-    let stdoutStub: SinonStub;
+describe('Sub-Command: fw', () => {
     let commandInstance: TestingModule;
+    let exitSpy: jest.SpyInstance;
+    let stdoutSpy: jest.SpyInstance;
+    let stderrSpy: jest.SpyInstance;
 
     beforeEach(async () => {
-        sandbox = sinon.createSandbox();
-        exitStub = sandbox.stub(process, 'exit');
-        stdoutStub = sandbox.stub(process.stdout, 'write');
         commandInstance = await CommandTestFactory.createTestingCommand({
             imports: [AppModule],
         }).compile();
+
+        exitSpy = jest.spyOn(process, 'exit').mockImplementation();
+        stdoutSpy = jest.spyOn(process.stdout, 'write').mockImplementation();
+        stderrSpy = jest.spyOn(process.stderr, 'write').mockImplementation();
     });
 
-    afterEach(() => {
-        sandbox.restore();
+    afterEach(async () => {
+        exitSpy.mockRestore();
+        stdoutSpy.mockRestore();
+        stderrSpy.mockRestore();
     });
 
-    function assertHelpText(text: string): void {
-        text = replaceSingleSpace(text);
-        assert(text.includes('Firewall utilities for developers'), 'Missing command description');
-        assert(text.includes('-h, --help display help for command'), 'Missing help option flag');
-        assert(
-            text.includes("integ [options] Integrate your contracts with Ironblocks' firewall"),
-            'Missing firewall integrate command',
-        );
-    }
 
-    it("it should display firewall command's help", async () => {
+    it('displays usage information if no command is specified', async () => {
         await CommandTestFactory.run(commandInstance, ['fw']);
-        const helpText = stdoutStub.firstCall?.args[0];
-        const exitCode = exitStub.firstCall?.args[0];
-        assertHelpText(helpText);
-        assert.equal(exitCode, 0, 'process should exit with 0');
+
+        const commandOutput = stdoutSpy.mock.calls[0][0];
+        expect(commandOutput).toContain('Usage: ib fw [options] [command]');
     });
 
-    it("it should display firewall command's help with -h", async () => {
-        await CommandTestFactory.run(commandInstance, ['fw', '-h']);
-        const helpText = stdoutStub.firstCall?.args[0];
-        const exitCode = exitStub.firstCall?.args[0];
-        assertHelpText(helpText);
-        assert.equal(exitCode, 0, 'process should exit with 0');
+    it('displays the description in the usage information', async () => {
+        await CommandTestFactory.run(commandInstance, ['fw']);
+
+        const commandOutput = stdoutSpy.mock.calls[0][0];
+        expect(commandOutput).toContain(DESCRIPTION);
     });
 
-    it("it should display firewall command's help with --help", async () => {
-        await CommandTestFactory.run(commandInstance, ['fw', '--help']);
-        const helpText = stdoutStub.firstCall?.args[0];
-        const exitCode = exitStub.firstCall?.args[0];
-        assertHelpText(helpText);
-        assert.equal(exitCode, 0, 'process should exit with 0');
+    it('displays the name of the command in the usage information', async () => {
+        await CommandTestFactory.run(commandInstance, ['fw']);
+
+        const commandOutput = stdoutSpy.mock.calls[0][0];
+        expect(commandOutput).toContain(NAME);
+    });
+
+    it('displays an error message if an invalid command is specified', async () => {
+        await CommandTestFactory.run(commandInstance, ['fw', 'invalid-command']);
+
+        const commandOutput = stderrSpy.mock.calls[0][0];
+        expect(commandOutput).toContain('Invalid command: invalid-command');
+    });
+
+    it('exits with a non-zero exit code if an invalid command is specified', async () => {
+        await CommandTestFactory.run(commandInstance, ['fw', 'invalid-command']);
+        expect(exitSpy).toHaveBeenCalledWith(1);
     });
 });
