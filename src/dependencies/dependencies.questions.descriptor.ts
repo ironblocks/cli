803b5eeae125d305f48b8ff9e85c3be3c768647f
Uri Chandler <uri@ironblocks.com>
2024-03-08 20:51:37 +0200

    chore: refactor and tests for dependencies questions

---
+++ src/dependencies/dependencies.questions.descriptor.ts
@@ -0,0 +1,4 @@
+export const QUESTION_TYPE = 'confirm';
+export const QUESTION_NAME = 'installDependencies';
+export const QUESTION_SET_NAME = 'dependencies';
+export const QUESTION_MESSAGE = 'Do you want to install missing dependencies?';
--- src/dependencies/dependencies.questions.ts
+++ src/dependencies/dependencies.questions.ts
@@ -1,11 +1,17 @@
 import { Question, QuestionSet } from 'nest-commander';
+import {
+    QUESTION_MESSAGE,
+    QUESTION_NAME,
+    QUESTION_SET_NAME,
+    QUESTION_TYPE
+} from '@/dependencies/dependencies.questions.descriptor';
 
-@QuestionSet({ name: 'dependencies' })
+@QuestionSet({ name: QUESTION_SET_NAME })
 export class DependenciesQuestions {
     @Question({
-        type: 'confirm',
-        name: 'installDependencies',
-        message: 'Do you want to install missing dependencies?'
+        type: QUESTION_TYPE,
+        name: QUESTION_NAME,
+        message: QUESTION_MESSAGE
     })
     parseInstallDependencies() {}
 }
