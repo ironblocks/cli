43c93fe0bdf840f4cfcc560dd19e3950a15467ea
Uri Chandler <uri@ironblocks.com>
2024-03-08 20:03:50 +0200

    fix: missing files module import

--- src/project-info/project-info.module.ts
+++ src/project-info/project-info.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 
 import { ProjectInfoService } from '@/project-info/project-info.service';
+import { FilesModules } from '@/files/files.module';
 
 @Module({
+    imports: [FilesModules],
     providers: [ProjectInfoService],
     exports: [ProjectInfoService]
 })
