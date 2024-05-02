7fd168c19efea307ad5a5219ae55e91f553d0bfc
joYyHack <agalaktionov5490@gmail.com>
2024-05-02 18:48:12 +0300

    chore: temp deletion 2

--- src/multisig/network.questions.descriptor.ts
+++
@@ -1,5 +0,0 @@
-export const NETWORK_QUESTION_TYPE = 'list';
-export const NETWORK_QUESTION_NAME = 'chooseNetwork';
-export const NETWORK_QUESTION_SET_NAME = 'network';
-export const NETWORK_QUESTION_MESSAGE =
-    "CLI supports SAFE multisig addresses out of the box. To ensure the validity of the multisig address, please choose one of the available networks and the address will be validated by the SAFE service. Select 'skip' in case you don't have multisig!";
