d0b2d5224252023c06529e247f7d2706ce392b16
Senan Warnock <senanw95@gmail.com>
2025-07-22 07:16:48 +0200

    feat: add InitModule to VennModule imports for initialization functionality

--- src/venn/venn.module.ts
+++ src/venn/venn.module.ts
@@ -3,8 +3,9 @@ import { Module } from '@nestjs/common';
 import { LoggerModule } from '@/lib/logging/logger.module';
 import { EnableVennModule } from '@/venn/enable/enable.module';
 import { DisableVennModule } from '@/venn/disable/disable.module';
+import { InitModule } from '@/venn/init/init.module';
 
 @Module({
-    imports: [EnableVennModule, DisableVennModule, LoggerModule]
+    imports: [EnableVennModule, DisableVennModule, InitModule, LoggerModule]
 })
 export class VennModule {}
